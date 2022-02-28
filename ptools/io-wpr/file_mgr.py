f8d4da7407bdc02a763f1dff6757726559828b8f
Robert King <ro.king.eu@gmail.com>
2022-02-28 13:43:25 +0100

    adding of file manager to the bundle of library

---
+++ ptools/io-wpr/file_mgr.py
@@ -0,0 +1,52 @@
+# -----------------------------------------------------------
+# small wrapper for file handling 
+#
+# 2022 Robert KING, Berlin, Germany
+# email: ro.king.eu@gmail.com 
+# -----------------------------------------------------------
+
+import json
+"""
+class that handles json & text files 
+
+read & write methods
+"""
+class file_mgr:
+    def __init__(self):
+        pass
+    
+    def read(self, _filename):
+        """ reads text,csv files 
+        parameter:
+           filename  
+            
+        return:
+           file splitted in a list
+        """ 
+        l_file = open(_filename,"r")
+        l_bufferlist = l_file.readlines()
+        l_file.close()
+        return l_bufferlist
+
+    def write(self, _filename , _bufferlist):
+        """ writes text in a file
+            parameter:
+               list of strings to be dumped into a file    
+            return:
+               none
+        """
+        l_newfile = open(_filename,"w")
+        l_newfile.writelines(_bufferlist)
+        l_newfile.close()
+
+    def readjson(self,_json):
+        """ reads json files
+            parameter:
+               list of strings to be dumped into a file    
+            return:
+                a dictionnary that contains the content of json file 
+        """
+        l_data=None
+        with open(_json) as json_data:
+            l_data = json.load(json_data)
+        return l_data
